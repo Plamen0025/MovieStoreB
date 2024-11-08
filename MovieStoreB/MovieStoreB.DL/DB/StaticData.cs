954270b0a571c1e9ca8f4581693ef124b3d0dde3
Plamen0025 <karapeykovivaylo@gmail.com>
2024-11-09 00:35:48 +0200

    Add files via upload

---
+++ MovieStoreB/MovieStoreB.DL/DB/StaticData.cs
@@ -0,0 +1,18 @@
+﻿using MovieStoreB.Models.DTO;
+
+namespace MovieStoreB.DL.DB
+{
+    internal class StaticData
+    {
+        public static List<Movie> Movies = new List<Movie>
+        {
+            new Movie
+            {
+                ID = 1, Title = "The Shawshank Redemption"
+            },
+            new Movie
+            { ID = 2,Title = "The Godfather",
+            },
+        };   
+    }
+}
---
+++ MovieStoreB/MovieStoreB.Models/DTO/Movie.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MovieStoreB.Models.DTO
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }   
+
+        public int Year { get; set; }
+    }
+}
